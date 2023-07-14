fb685253e231de78580886368eb439171b61200a
smige1 <egimsikana@gmail.com>
2023-07-14 03:47:28 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,13 @@
 
 /**
  * main - Entry point
+ *
  * Return: 0 if exited properly, non-zero otherwise
  */
-int main()
+int main(void)
 {       
+
        puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 
 }       

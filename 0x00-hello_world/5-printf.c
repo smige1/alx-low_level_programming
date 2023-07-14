14ef39941203d9b87866235385bf3a107924bebf
smige1 <egimsikana@gmail.com>
2023-07-14 03:11:50 -0700

    Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {       
-        puts("with proper grammar, but the outcome is a piece of art,\n");
+        printf("with proper grammar, but the outcome is a piece of art,\n");
         return (0);
 }       
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {       

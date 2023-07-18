1be45d7c757d023ccf38ebd8a7796ac7d25616f2
smige1 <egimsikana@gmail.com>
2023-07-18 12:48:29 -0700

    NESTED LOOP COMPLETED

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,17 @@
 * Return: The last digit of n
 */
 
-int print_last_digit(int n)
+int print_last_digit(int c)
 {
-int last_digit;
+	int last;
 
-last_digit = n % 10;
-putchar(last_digit + '0');
-return (last_digit);
+	last = 0;
+
+	last = c % 10;
+
+	if (last < 0)
+		last *= -1;
+	_putchar(last + '0');
+
+	return (last);
 }

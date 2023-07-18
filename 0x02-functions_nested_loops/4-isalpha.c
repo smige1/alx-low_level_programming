9025185e4731cbbf6d61c8de3d00358ed2c3910e
smige1 <egimsikana@gmail.com>
2023-07-18 11:47:44 -0700

    NESTED LOOP COMPLETED

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 /**
-*main -print _putchar
-*is writen by smige
-*return: 0
+* _isalpha - checks if a character is alphabetic
+*
+* @c: The character to check
+*
+* Return: 1 if c is alphabetic, 0 otherwise
 */
-int _isalpha(int c) {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-return 1;
-} else {
-return 0;
+
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
 }
 }

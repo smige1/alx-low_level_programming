e0b4f24509b36bbee8c75e2ff8c529669354b902
smige1 <egimsikana@gmail.com>
2023-07-18 12:22:14 -0700

    NESTED LOOP COMPLETED

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 /**
-* main -print _putchar
-* is writen by smige
-* return: 0
+* print_sign - prints the sign of a number
+*
+* @n: The number to print the sign of
+*
+* Return: 1 if n is positive, 0 if n is zero, and -1 if n is negative
 */
 
-int print_sign(int n) {
-if (n > 0) {
+int print_sign(int n)
+{
+if (n > 0)
+{
 putchar('+');
-return 1;
-} else if (n == 0) {
+return (1);
+}
+else if (n == 0)
+{
 putchar('0');
-return 0;
-} else {
+return (0);
+}
+else
+{
 putchar('-');
-return -1;
+return (-1);
 }
 }

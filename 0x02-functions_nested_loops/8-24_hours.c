c1aa692ae493578b755d5f9e6e9ae885be4102c8
smige1 <egimsikana@gmail.com>
2023-07-18 13:38:46 -0700

    NESTED LOOP COMPLETED

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 /**
-* main -print _putchar
-* is writen by smige
-* return: 0
+* jack_bauer - prints the 24-hour clock
+*
+* Return: void
 */
 
 void jack_bauer(void) {
 int hours;
 int minutes;
 
-for (hours = 0; hours <= 23; hours++) {
-for (minutes = 0; minutes <= 59; minutes++) {
+for (hours = 0; hours <= 23; hours++)
+{
+for (minutes = 0; minutes <= 59; minutes++)
+{
 printf("%02d:%02d\n", hours, minutes);
 }
 }

53833a057ad638add6df08095056af6d996ed8bb
smige1 <egimsikana@gmail.com>
2023-07-18 11:16:29 -0700

    NESTED LOOP COMPLETED

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* print_alphabet_x10 - prints 10 times the alphabet, 
+* print_alphabet_x10 - prints 10 times the alphabet,
 * followed by a new line
 *
 * Return: void
@@ -11,10 +11,11 @@ void print_alphabet_x10(void)
 	int i;
 	char ch;
 	i = 0;
+
 		while (i < 10)
 		{
 		ch = 'a';
-	while (ch <= 'z') 
+	while (ch <= 'z')
 	{
 	putchar(ch);
 	ch++;

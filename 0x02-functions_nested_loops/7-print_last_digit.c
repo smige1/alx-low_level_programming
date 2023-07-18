7cb6e076559ae5c02bc0fa994db6c462bf103df3
smige1 <egimsikana@gmail.com>
2023-07-18 12:46:21 -0700

    NESTED LOOP COMPLETED

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 /**
-* main -print _putchar
-* is writen by smige
-* return: 0
+* print_last_digit - prints the last digit of a number
+*
+* @n: The number to print the last digit of
+*
+* Return: The last digit of n
 */
 
-int print_last_digit(int n) {
+int print_last_digit(int n)
+{
 int last_digit;
 
 last_digit = n % 10;
 putchar(last_digit + '0');
-return last_digit;
+return (last_digit);
 }

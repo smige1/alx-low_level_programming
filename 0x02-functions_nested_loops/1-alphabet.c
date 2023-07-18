0b82a34e43c07ede7ce7ed6cd22d6d6336090cf1
smige1 <egimsikana@gmail.com>
2023-07-18 10:38:01 -0700

    NESTED LOOP COMPLETED

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,10 +5,12 @@
 * return: 0
 */
 
-void print_alphabet(void) {
+void print_alphabet(void)
+{
 int i;
 
-for (i = 97; i <= 122; i++) {
+for (i = 97; i <= 122; i++)
+{
 putchar(i);
 }
 putchar('\n');

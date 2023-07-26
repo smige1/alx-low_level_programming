40fc3d8320885f69fcf2859af025e1d65f23aaa8
smige1 <egimsikana@gmail.com>
2023-07-26 13:12:30 -0700

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,3 +20,14 @@ char *cap_string(char *str)
 	}
 	return str;
 }
+
+int main(void)
+{
+	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 01234hello world\thello world.hello world\n";
+	char *ptr;
+
+	ptr = cap_string(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return 0;
+}

6ef23c945169d08d18018aa594d62c48a0146ade
smige1 <egimsikana@gmail.com>
2023-07-26 13:09:06 -0700

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
-#include <ctype.h>
+#include "main.h"
+//A program that capitalizes all words of a string
 
-char *cap_string(char *str) {
-  int i;
-  for (i = 0; str[i] != '\0'; i++) {
-    if (i == 0 || !isalpha(str[i - 1])) {
-      if (islower(str[i])) {
-        str[i] = toupper(str[i]);
-      }
-    }
-  }
-  return str;
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while(str[i] != '\0')
+	{
+		if((str[i] == ' ' || str[i] == '.') ||(str[i] == '\n' || str[i] =='\t'))
+		{	i++;
+			if(str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+		else
+			i++;
+	}
+	return str;
 }

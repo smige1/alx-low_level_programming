f68d103f87cebd5d4e27fe73235081f1e0b25605
smige1 <egimsikana@gmail.com>
2023-08-10 06:39:19 -0700

    Done

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,10 +9,13 @@
  * A pointer to the allocated memory. If malloc fails, the function terminates
  * the process with a status value of 98.
  */
-void *malloc_checked(unsigned int b) {
+void *malloc_checked(unsigned int b)
+{
 void *ptr = malloc(b);
-if (ptr == NULL) {
-printf(stderr, "Error: malloc failed\n");
+
+if (ptr == NULL) 
+{
+fprintf(stderr, "Error: malloc failed\n");
 exit(98);
 }
 return ptr;

98ec1a37c3106d0af853469616ef8d0f4688c968
smige1 <egimsikana@gmail.com>
2023-08-10 06:32:52 -0700

    Done

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -12,7 +12,7 @@
 void *malloc_checked(unsigned int b) {
 void *ptr = malloc(b);
 if (ptr == NULL) {
-fprintf(stderr, "Error: malloc failed\n");
+printf(stderr, "Error: malloc failed\n");
 exit(98);
 }
 return ptr;

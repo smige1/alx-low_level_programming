caa9b01f93c28986e295ac96f3e79c79ed958f39
smige1 <egimsikana@gmail.com>
2023-08-10 08:43:49 -0700

    Done

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,47 +1,19 @@
 #include "main.h"
 
 /**
- * string_nconcat - a function that concatenates two strings.
+ * malloc_checked - cause normal process termination with a status value of 98
  *
- * @u1: first char
- * @u2: secound char
- * @p: unsigned int
+ * @b: allocated memory
  *
- * Return: If the function fails, it should return NULL
+ * Return: a pointer to the allocated memory
  */
-char *string_nconcat(char *u1, char *u2, unsigned int p)
+void *malloc_checked(unsigned int b)
 {
-	unsigned int x, y, z;
-	char *s;
+	void *i;
 
-	if (u1 == NULL)
-	{
-		x = 0;
-	}
-	else
-	{
-		for (x = 0; u1[x]; ++x)
-		;
-	}
-	if (u2 == NULL)
-	{
-		y = 0;
-	}
-	else
-	{
-		for (y = 0; u2[y]; ++y)
-		;
-	}
-	if (y > p)
-		y = p;
-	s = malloc(sizeof(char) * (x + y + 1));
-	if (u == NULL)
-		return (NULL);
-	for (z = 0; z < x; z++)
-		u[z] = u1[z];
-	for (z = 0; z < y; z++)
-		u[z + x] = u2[z];
-	s[x + y] = '\0';
-	return (u);
+	i = malloc(b);
+	if (i == NULL)
+		exit(98);
+	return (i);
+
 }
-

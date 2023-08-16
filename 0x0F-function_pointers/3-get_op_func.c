80d98d9fdae0c7ebec3bd6f68e30cd7d21a8e4d4
smige1 <egimsikana@gmail.com>
2023-08-16 13:18:05 -0700

    Done

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,38 +1,32 @@
 #include "3-calc.h"
-#include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * get_op_func - returns a pointer to the function corresponing to char entered
- *
- * @s: the char entered
- * Return: ponter to the appropriate function
- */
-
+  * get_op_func - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
 	};
 	int i = 0;
 
-	while (ops[i].op[0] != s[0])
+	while (i < 5)
 	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+
 		i++;
-
 	}
 
-	if (i == 5)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	return (ops[i].f);
+	return (0);
 }

cd95d251cd817d424972c972381de0c876c9cfb5
smige1 <egimsikana@gmail.com>
2023-08-24 16:13:09 -0700

    Done

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -14,11 +14,11 @@
  *
  * Description: singly linked list node structure
  */
-typedef struct list_p
+typedef struct list_s
 {
     char *str;
     unsigned int len;
-    struct list_p *next;
+    struct list_s *next;
 } list_t;
 
 

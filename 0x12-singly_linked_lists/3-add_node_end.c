31bd1fbf7025b9740e503432abd3bbc2f4c93074
smige1 <egimsikana@gmail.com>
2023-08-25 03:43:34 -0700

    Done

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,53 +1,49 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a node at the end of the linked list
+ * add_node_end - adds a node at the enf of a list
  *
- * @head: pointer to the first node of the list
- * @str: string to add
+ * @head: pointer of the first element of the list
+ * @str: the string to include in the node
  *
- * Return: NULL if it fails / starting addr of the list
-*/
+ * Return: the pointer to the new node
+ */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *temp;
-	size_t str_len = 0;
-
-	/*set strig length to 0 if its NULL*/
-	if (str == NULL)
-		str_len = 0;
-
-	/*count length of string*/
-	while (str[str_len] != '\0')
-		str_len++;
+	unsigned int mylen;
+	list_t *new_node;
+	list_t *last = *head;
 
 	new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = str_len;
-	new_node->next = NULL;
 
-	/*copy our starting node pointer to temp*/
-	temp = *head;
+	if (str == NULL)
+		return (NULL);
 
-	/*if temp is NULL place our new_node at the beginning of the list*/
-	/*else if its not null we place our new_node to temp->next hence*/
-	/*adding our new_node at the end of the list*/
-	if (temp == NULL)
+	for (mylen = 0; str[mylen] != '\0'; mylen++)
+	{}
+	new_node->len = mylen;
+
+	if (*head == NULL)
 		*head = new_node;
+
 	else
 	{
-		/**
-		 * we iterate pointer next addr if its not NULL our temp addr
-		 * points to the next addr or the next node.
-		*/
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new_node;
+		while (last->next != 0)
+		{
+			last = last->next;
+		}
+		last->next = new_node;
+
+		new_node->next = NULL;
 	}
 
-	return (*head);
+	return (new_node);
+
+
 }

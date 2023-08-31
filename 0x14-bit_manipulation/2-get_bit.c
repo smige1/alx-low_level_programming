37b26e05f99fbb00b64cca0735fb9c67d819cf9f
smige1 <egimsikana@gmail.com>
2023-08-31 15:39:45 -0700

    Done

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * get_bit - value of a bit at a given index.
+ * @n: The bit.
+ * @index: The indices start at 0.
+ *
+ * Return: If an error occurs-1.
+ *
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+/* Check if the index is valid.*/
+if (index >= sizeof(unsigned long int) * 8)
+
+return (-1);
+
+
+/* Create a mask with the bit at the specified index set to 1.*/
+if ((n & (1 << index)) == 0)
+		return (0);
+	/* Shift the mask to the left by the specified index.*/
+}

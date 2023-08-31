c8ae2e145ebad87423c7122d56acd0295d83f682
smige1 <egimsikana@gmail.com>
2023-08-31 14:20:14 -0700

    Done

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,20 +6,21 @@
  * @n: The num
  * @m: The number to flip n
  *
- * Return: The necessary number of bits from n to m.
+ * Return: number of bits from n to m.
  */
 
-unsigned int flip_bits(unsigned long int n, unsigned long int m) {
-  /* Create a variable to store the number of bits that need to be flipped.*/
-  unsigned int flips = 0;
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+/* Create a variable to store the number of bits that need to be flipped.*/
 
-  // Loop through each bit in the numbers.
-  for (unsigned int i = 0; i < sizeof(unsigned long int) * 8; i++) {
-    /* If the bits are different, increment the number of flips.*/
-    if ((n & (1 << i)) != (m & (1 << i))) {
-      flips++;
-    }
-  }
+	unsigned long int xor = n ^ m, bits = 0;
 
-  /* Return the number of flips.*/
-  return flips;}
+/* Loop through each bit in the numbers.*/
+	while (xor > 0)
+	{
+		bits += (xor & 1);
+		xor >>= 1;
+	}
+ /* If the bits are different, increment the number of flips.*/
+return (bits);
+}

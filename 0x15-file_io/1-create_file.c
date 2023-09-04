570a639dfd018bb4adf76c3d48cc84b1525e20a1
smige1 <egimsikana@gmail.com>
2023-09-04 12:02:57 -0700

    Done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,13 +1,36 @@
 #include "main.h"
 
 /**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to be printed in binary.
- */
-void print_binary(unsigned long int n)
+ * create_file - a function that creates a file
+ *
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ *
+ * Return: 1 on success OR -1 on faliure
+*/
+int create_file(const char *filename, char *text_content)
 {
-	if (n > 1)
-		print_binary(n >> 1);
+	int file, write_status, words = 0;
 
-	_putchar((n & 1) + '0');
+	if (filename == NULL) /*check if filename is present*/
+		return (-1);
+
+	/*open file by creating it and if it exists write but truncate to 0*/
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1) /*check if file creation was a success*/
+		return (-1);
+
+	if (text_content) /*write content to file if its not NULL*/
+	{
+		while (text_content[words] != '\0') /*find number of words*/
+			words++;
+
+		/*write to file*/
+		write_status = write(file, text_content, words);
+		if (write_status == -1) /*check if write was a success*/
+			return (-1);
+	}
+
+	close(file); /*close file*/
+	return (1);
 }
